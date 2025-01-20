641ca6c585a510652f83e291c24afd63d31cab47
Abdulmajeed Rashad <gr53lh6f@students.codeinstitute.net>
2025-01-20 09:52:29 +0000

    Migrations applied, procfile configured, secrets os setup to ensure secret safety

--- django_habit/settings.py
+++ django_habit/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
+if os.path.isfile("env.py"):
+   import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r20d$2^&@f*f@8ub1!c2&=)jfe1fa1-)zxd!u#v(^-dn(im%b%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-whattheyonn-djangohabit-fus5bad9qy1.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-whattheyonn-djangohabit-fus5bad9qy1.ws.codeinstitute-ide.net', 'habitracker-e791def7e3c6.herokuapp.com', '8000-whattheyonn-djangohabit-skszm1qg2nu.ws.codeinstitute-ide.net']
 CSRF_TRUSTED_ORIGINS = ['https://*.codeinstitute-ide.net', 'https://*.herokuapp.com']
 
 # Application definition
@@ -74,12 +80,16 @@ WSGI_APPLICATION = 'django_habit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
 
 
 # Password validation
