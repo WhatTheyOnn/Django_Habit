5d8861cfba0777407ad5875d825b356eb243ee03
Abdulmajeed Rashad <gr53lh6f@students.codeinstitute.net>
2025-01-24 15:34:28 +0000

    Made the forms an action that can take place on the website and made if statements to show what they added on the screen

--- habit_tracker/views.py
+++ habit_tracker/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
 
 from .models import *
@@ -8,6 +8,12 @@ def index(request):
     habit = Habit.objects.all()
 
     form = Habitform()
+
+    if request.method == 'POST':
+        form = Habitform(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
 
     context = {'habits': habit, 'form': form}
     return render(request, 'Habit_tracker/list.html', context)
