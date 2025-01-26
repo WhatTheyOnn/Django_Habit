8b9a4c158ede7d779b4078cae7e83956c6bbe977
Abdulmajeed Rashad <gr53lh6f@students.codeinstitute.net>
2025-01-26 16:21:10 +0000

    added flash logs if username or login is incorrect or if passwords do not match also added a redicrect when logged in successfully

--- habit_tracker/views.py
+++ habit_tracker/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from .models import *
 from .forms import *
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
+
 # Create your views here.
 from .forms import CreateUserForm
 
@@ -15,12 +20,29 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+
             return redirect('login')
 
     context = {'form': form}
     return render(request, 'Habit_tracker/Accounts/register.html', context)
 
 def loginPage(request):
+
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Username OR password is incorrect')
+            return render(request, 'Habit_tracker/Accounts/login.html')
     context = {}
     return render(request, 'Habit_tracker/Accounts/login.html', context)
 
