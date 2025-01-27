3eab99a21f21f128faafde26809d4576f22a312d
Abdulmajeed Rashad <gr53lh6f@students.codeinstitute.net>
2025-01-27 14:53:53 +0000

    Edited max character length on models.py for the habit titles to be 15

--- habit_tracker/models.py
+++ habit_tracker/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Habit(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=15)
     completed = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
