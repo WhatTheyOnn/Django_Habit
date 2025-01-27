e413590243084d131372d561637de54df521b2a8
Abdulmajeed Rashad <gr53lh6f@students.codeinstitute.net>
2025-01-27 17:56:43 +0000

    changed debug to false

--- django_habit/settings.py
+++ django_habit/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-whattheyonn-djangohabit-fus5bad9qy1.ws.codeinstitute-ide.net', 'habitracker-e791def7e3c6.herokuapp.com', '8000-whattheyonn-djangohabit-skszm1qg2nu.ws.codeinstitute-ide.net', '127.0.0.1','localhost']
 CSRF_TRUSTED_ORIGINS = ['https://*.codeinstitute-ide.net', 'https://*.herokuapp.com']
